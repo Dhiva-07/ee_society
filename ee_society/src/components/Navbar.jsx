03cf1b17bb16a12e33186b78bfe5ed4c9d3bae84
Dhiva-07 <dhiva21112004@gmail.com>
2025-03-28 17:24:13 +0530

    Merged Changes Navbar

--- ee_society/src/components/Navbar.jsx
+++ ee_society/src/components/Navbar.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { handleSuccess } from "../utils";
 import { ToastContainer } from "react-toastify";
-import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
@@ -13,8 +11,15 @@ function Navbar({ dropdownOpen, setDropdownOpen }) {
   const PF = process.env.REACT_APP_PUBLIC_URL2;
   const navigate = useNavigate();
 
+  useEffect(() => {
+    setIsAdmin(localStorage.getItem("isAdmin") === "true");
+  }, []);
+
   const handleLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("loggedInUser");
+    localStorage.removeItem("isAdmin");
+    localStorage.removeItem("email");
     handleSuccess("User Logged Out");
     setTimeout(() => {
       navigate("/login");
@@ -57,7 +62,7 @@ function Navbar({ dropdownOpen, setDropdownOpen }) {
           <li>
             <Link to="/community">Community</Link>
           </li>
-          {user.isAdmin && (
+          {isAdmin && (
             <li>
               <Link to="/admin">Admin</Link>
             </li>
